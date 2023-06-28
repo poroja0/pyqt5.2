cb9dad66814cf611d704e310be080ea218e160d1
poroja0 <112116876+poroja0@users.noreply.github.com>
2023-06-28 19:03:06 +0300

    Update fin_win.py

--- fin_win.py
+++ fin_win.py
@@ -14,7 +14,7 @@ from instr import *
 
      
 class Result_win(QWidget):
-   def __init__(self):
+   def __init__(self, age,p1,p2,p3):
        ''' окно, в котором располагается приветствие '''
        super().__init__()
 
@@ -24,6 +24,7 @@ class Result_win(QWidget):
 
 
        # создаём и настраиваем графические элементы:
+       self.results(age,p1,p2,p3)
        self.initUI()
 
 
@@ -33,10 +34,14 @@ class Result_win(QWidget):
        # старт:
        self.show()
 
+   def results(self, age,p1,p2,p3):
+      self.index = (4*(int(age)+ int(p1)+int(p2)+int(p3))-200)/10
+
+
 
    def initUI(self):
        ''' создаёт графические элементы '''
-       self.text1 = QLabel(txt_index)
+       self.text1 = QLabel(txt_index + str(self.index))
       
        self.text2 = QLabel(txt_workheart)
        
