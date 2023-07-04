f900aea0737d5295a93de79d1c75943f57c70505
poroja0 <112116876+poroja0@users.noreply.github.com>
2023-07-04 20:21:56 +0300

    Add files via upload

--- fin_win.py
+++ fin_win.py
@@ -24,8 +24,8 @@ class Result_win(QWidget):
 
 
        # создаём и настраиваем графические элементы:
-       self.results(age,p1,p2,p3)
-       self.initUI()
+      
+       self.initUI(age,p1,p2,p3)
 
 
       
@@ -34,16 +34,117 @@ class Result_win(QWidget):
        # старт:
        self.show()
 
+   
+   
+   
+   
+   
    def results(self, age,p1,p2,p3):
-      self.index = (4*(int(age)+ int(p1)+int(p2)+int(p3))-200)/10
+
+      age = int(age)
+      self.index = (4*(int(p1)+int(p2)+int(p3))-200)/10
+      if age >=7:
+           
+            if age <=8:
+                if self.index >=6.4:
+                    
+                    if self.index <=11.9:
+                        return txt_res4
+                    elif self.index <=16.9:
+                        return txt_res3
+                    elif self.index <=20.9:
+                        return txt_res2
+                    else:
+                        return txt_res1
+
+                
+                else:
+                    self.setStyleSheet("background-color: ")
+                    return txt_res5
+
+            elif age <=10:
+
+                if self.index >=4.9:
+                    
+                    if self.index <=10.4:
+                        return txt_res4
+                    elif self.index <=15.4:
+                        return txt_res3
+                    elif self.index <=19.4:
+                        return txt_res2
+                    else:
+                        return txt_res1
+
+                
+                else:
+                    return txt_res5
+
+            elif age <=12:
+
+                if self.index >=3.4:
+                    
+                    if self.index <=8.9:
+                        return txt_res4
+                    elif self.index <=13.9:
+                        return txt_res3
+                    elif self.index <=17.9:
+                        return txt_res2
+                    else:
+                        return txt_res1
+
+                
+                else:
+                    return txt_res5
+
+            elif age <=14:
+
+                if self.index >=1.9:
+                    
+                    if self.index <=7.4:
+                        return txt_res4
+                    elif self.index <=12.4:
+                        return txt_res3
+                    elif self.index <=16.4:
+                        return txt_res2
+                    else:
+                        return txt_res1
+
+                
+                else:
+                    return txt_res5
+
+            else:
+
+                if self.index >=0.4:
+                    
+                    if self.index <=5.9:
+                        return txt_res4
+                    elif self.index <=10.9:
+                        return txt_res3
+                    elif self.index <=14.9:
+                        return txt_res2
+                    else:
+                        return txt_res1
+
+                
+                else:
+                    return txt_res5
 
 
 
-   def initUI(self):
+                
+            
+      else:
+          return 'вы не подходите под параметры'
+
+
+
+   def initUI(self, age,p1,p2,p3):
        ''' создаёт графические элементы '''
-       self.text1 = QLabel(txt_index + str(self.index))
+       
       
-       self.text2 = QLabel(txt_workheart)
+       self.text2 = QLabel(txt_workheart + self.results(age,p1,p2,p3)) 
+       self.text1 = QLabel(txt_index + str(self.index))
        
 
        
